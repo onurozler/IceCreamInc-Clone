b40c59cc0ec616fbb170f8611648822e01ffa3f5
onurozler <onur.ozler@ug.bilkent.edu.tr>
2020-05-04 14:08:28 +0300

    Initial Commit

---
+++ Assets/IceCreamTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BezierSolution;
+using UnityEngine;
+
+public class IceCreamTest : MonoBehaviour
+{
+    public BezierSpline _bezierSpline;
+    public GameObject iceCream;
+
+    private void Start()
+    {
+       // iceCream.transform.position =_bezierSpline.GetPoint(0.7f);
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(iceCream,transform);
+            // https://www.reddit.com/r/Unity3D/comments/g0e1lv/how_can_i_create_cream_like_in_ice_cream_inc/
+            // Bezier Curves
+            
+        }
+    }
+}
